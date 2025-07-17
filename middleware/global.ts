e40a8bc0cf9d1476bc3a1b09413f6182710ff97d
stepa_eliz <ODESSA007>
2025-07-16 23:40:35 -0700

    Middleware update 2

--- middleware/global.ts
+++ middleware/global.ts
@@ -1,5 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
   const publicPaths = ['/', '/login']
+  console.log('➡️ middleware path:', to.path)
 
   // `to.path` is the raw URL path; ensure it matches exactly
   const isPublic = publicPaths.includes(to.path)
